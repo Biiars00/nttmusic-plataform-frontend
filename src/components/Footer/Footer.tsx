d8b05edf68bb07dd5a11dccb4d421169f990681f
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-23 23:48:13 -0300

    feat: Creating a footer component.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <>
+        <footer className="flex flex-col justify-center h-16 text-center items-center">
+            <p className="text-sm">© 2025 All rights reserved.</p>
+            <p className="text-sm text-gray-300">Developer by Biiars</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
