2086655cd6bf18f715b6b92adfab12396c058d08
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-23 23:50:18 -0300

    refactor: refactoring of the registration page.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { register } from '../services/useUsersService';
+import Header from '../components/Header/Header';
+import Form from '../components/Form/Form';
+import InputForm from '../components/InputForm/InputForm';
+import Button from '../components/Button/Button';
+import Footer from '../components/Footer/Footer';
+import { toast } from 'react-toastify';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -10,22 +16,64 @@ const Register = () => {
     e.preventDefault();
     try {
       await register(form);
-      alert('Cadastro realizado com sucesso!');
+      toast.success('Cadastro realizado com sucesso!');
+
       navigate('/user/login');
     } catch (error: any) {
-      alert(error?.response?.data?.message || 'Erro no cadastro');
+      toast.error(error?.response?.data?.message || 'Erro no cadastro');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Cadastro</h2>
-      <input placeholder="Nome" value={form.userName} onChange={e => setForm({ ...form, userName: e.target.value })} />
-      <input placeholder="Email" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} />
-      <input type="password" placeholder="Senha" value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} />
-      <button type="submit">Cadastrar</button>
-      <p>Já tem conta? <Link to="/">Faça login</Link></p>
-    </form>
+    <>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <div className="flex-1 flex justify-center items-center">
+        <Form 
+          name="Cadastro"
+          onSubmit={handleSubmit}>
+          <InputForm 
+            label="Nome"
+            name="userName"
+            type="text"
+            value={form.userName}
+            placeholder="Nome"
+            onChange={e => setForm({ ...form, userName: e.target.value })}
+          />
+
+          <InputForm 
+            label="Email"
+            name="email"
+            type="text"
+            value={form.email}
+            placeholder="Email"
+            onChange={e => setForm({ ...form, email: e.target.value })}
+          />
+
+          <InputForm 
+            label="Senha"
+            name="password"
+            type="text"
+            value={form.password}
+            placeholder="Senha"
+            onChange={e => setForm({ ...form, password: e.target.value })}
+          />
+
+          <Button 
+            className="w-[200px] p-4 mt-6 rounded bg-gray-800 hover:bg-gray-600 hover:duration-700" 
+            classNameText="text-white font-bold text-lg"
+            type="submit"
+          >
+            Cdastrar
+          </Button>
+
+        </Form>
+      </div>
+
+      <Footer />
+    </div>
+    </>
   );
 };
 export default Register;
