b514499bef14a8858a64fd4f3853e1bd325de33f
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-23 23:48:53 -0300

    feat: creating the header component.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex items-center pl-4 text-sans bg-white h-16 shadow-md">
+            <h1 className="font-bold text-xl font-heading">🎵 NttMusic</h1>
+        </header>
+    )
+}
+
+export default Header;
