b036ed4af2205dc1e39fa06acbcf6b7a1774690c
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-25 17:29:19 -0300

    refactor: update Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
-const Header = () => {
+import { ReactNode } from "react";
+
+interface IHeaderProps {
+    nameLogo: string;
+    children?: ReactNode;
+}
+
+const Header = ({ nameLogo, children }: IHeaderProps) => {
     return (
         <header className="flex items-center pl-4 text-sans bg-white h-16 shadow-md">
-            <h1 className="font-bold text-xl font-heading">🎵 NttMusic</h1>
+            <h1 className="font-bold text-xl font-heading">{nameLogo}</h1>
+            {children}
         </header>
     )
 }
