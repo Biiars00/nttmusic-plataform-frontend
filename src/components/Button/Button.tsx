eb308536d11acd3e0e5a82758e9910376054c86f
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-05-25 17:28:14 -0300

    refactor: event change.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,23 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
 interface ButtonProps {
     type?: 'button' | 'submit' | 'reset';
     children: ReactNode;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
     className: string;
     classNameText: string;
 }
 
-const Button = ({ type, children, className, classNameText }: ButtonProps) => {
+const Button = ({ type, children, className, classNameText, onClick }: ButtonProps) => {
     return (
         <>
-        <button 
-            className={className}
-            type={type}
-        >
-            <p className={classNameText}>{children}</p>
-        </button>
+            <button 
+                className={className}
+                type={type}
+                onClick={onClick}
+            >
+                <p className={classNameText}>{children}</p>
+            </button>
         </>
     )
 }
